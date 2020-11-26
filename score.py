9f5de96623990d0bda58307c2bc95140fe918393
PhilippRamjoue <37185708+PhilippRamjoue@users.noreply.github.com>
2020-11-26 14:15:26 +0000

    Add files via upload

--- score.py
+++ score.py
@@ -1,21 +1,18 @@
 import json
-import pickle
 import numpy as np
-import pandas as pd
-import azureml.train.automl
-from sklearn.externals import joblib
+import os
+import pickle
+import joblib
 from azureml.core.model import Model
+import azureml.train.automl
+
 
 def init():
     global model
 
-    model_path = Model.get_model_path(model_name='AutoML37a31f86512')
+    model_path = Model.get_model_path(model_name='best_automl_model.pkl')
+
     model = joblib.load(model_path)
-
-    #model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
-    #model = Model.get_model_path('best_automl_model')
-
-    #model = joblib.load(model_path)
 
 def run(raw_data):
 
